27aa3a829ecb71bf2afeef255bd9d47265fb2c96
Wojciech <kubatwojtek5@gmail.com>
2025-04-07 17:08:17 +0200

    sidebar menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,21 @@
 const menuToggle = document.querySelector('.toggle');
 const showcase = document.querySelector('.showcase');
-
+// const menu = document.querySelector('.menu');
 menuToggle.addEventListener('click', () => {
   menuToggle.classList.toggle('active');
   showcase.classList.toggle('active');
+  // menu.classList.toggle('active');
 })
 
 
-let scrollbutton = document.getElementById("scrollBtn");
-
-
+let scrollButton = document.getElementById("scrollBtn");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    scrollbutton.style.display = "flex";
+    scrollButton.style.display = "flex";
   } else {
-    scrollbutton.style.display = "none";
+    scrollButton.style.display = "none";
   }
 }
 
@@ -24,3 +23,16 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+// function openNav() {
+//   document.getElementById("sideMenu").style.display = "flex";
+//   document.getElementById("toggleBtn").style.backgroundImage = "url('../images/close.jpg')";
+//   document.getElementById("toggleBtn").onclick = closeNav;
+//   document.getElementById("sideMenu").style.transition = "0.4s";
+// }
+
+// function closeNav() {
+//   document.getElementById("sideMenu").style.display = "none";
+//   document.getElementById("toggleBtn").style.backgroundImage = "url('../images/menu.jpg')";
+//   document.getElementById("toggleBtn").onclick = openNav;
+// }
